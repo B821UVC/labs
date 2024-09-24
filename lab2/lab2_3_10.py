f72c9ec801a2468c9d74b2bdde6287ece121e229
B821UVC <m2408839@edu.misis.ru>
2024-09-24 17:52:01 +0300

    Update lab2_3_10.py

--- lab2/lab2_3_10.py
+++ lab2/lab2_3_10.py
@@ -1,8 +1,12 @@
+'''такое же условие как 2_10'''
+
 x=0
 while True:
     try:
         flag=True
         for s in range(4):
+
+            
             if int(int(input()))<4 and flag:
                 x+=1
                 flag=False
