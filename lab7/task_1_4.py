fde19ddb47e56b780c6eb525a7e59a36509844b6
B821UVC <m2408839@edu.misis.ru>
2024-12-20 14:58:24 +0300

    Update task_1_4.py

--- lab7/task_1_4.py
+++ lab7/task_1_4.py
@@ -10,14 +10,7 @@ class competiton_participant:
         else:
             return self.attempt2
 
-lst_of_participants = []
-for i in range(4):
-    print("Введите в порядке: имя, возраст, попытка 1, попытка 2. Каждое значение с новой строки")
-    name = input()
-    age = int(input())
-    attempt1 = int(input())
-    attempt2 = int(input())
-    lst_of_participants.append(competiton_participant(name, age, attempt1, attempt2))
+lst_of_participants = [competiton_participant("Igor", 22, 200, 180), competiton_participant("Winner", 25, 230, 168), competiton_participant("nikita", 18, 194, 178), competiton_participant("kolya", 20, 177, 188)]
 lst_of_participants.sort(key = lambda x: x.get_best_attempt(), reverse = True)
 for i in range(4):
     print(i+1, lst_of_participants[i].name, lst_of_participants[i].get_best_attempt())
