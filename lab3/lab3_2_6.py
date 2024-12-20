c308a84a94cd0b61df5d96a2a58aac32f98fe77d
B821UVC <m2408839@edu.misis.ru>
2024-12-20 12:51:54 +0300

    Update lab3_2_6.py

--- lab3/lab3_2_6.py
+++ lab3/lab3_2_6.py
@@ -2,17 +2,24 @@
 '''Задан одномерный массив и число Р. Включить элемент, рав-
 ный Р, после того элемента массива, который наиболее близок к сред-
 нему значению его элементов.'''
-#arr = [float(x) for x in input().split()]
 arr = [0, 3, 99, 15, 16, 55, 44, -31, 50]
-arr2 = [0, 9, 9, 17.3, -16.3, -14, 73, 58]
-P = float(input())
+#arr2 = [0, 9, 9, 17.3, -16.3, -14, 73, 58]
+P = float(input("Введите число которое необходимо включить в список:"))
 
 delta = 10**21
 need_ps = 0
+
+avg = 0
+for i in range(len(arr)):
+    avg+=arr[i]
+avg = avg/len(arr)
+
+
 for ps,i in enumerate(arr):
-    if (abs(P-i)<delta):
+    if (abs(avg-i)<delta):
         need_ps = ps
-        delta = abs(P-i)
+        delta = abs(avg-i)
 
 arr = arr[:need_ps+1] +[P]+ arr[need_ps+1:] 
+print("Среднее значение: ", avg)
 print(arr)
